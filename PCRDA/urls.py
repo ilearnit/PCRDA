b613567a7ecc935f417b44cbc0fe4c12e74eee75
ilearnit <helloworld.c@outlook.com>
2018-10-29 17:49:12 +0800

    refactor Production

--- PCRDA/urls.py
+++ PCRDA/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', include('home.urls')),
@@ -23,3 +26,6 @@ urlpatterns = [
     path('api-auth', include('rest_framework.urls')),
     # path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns() 
